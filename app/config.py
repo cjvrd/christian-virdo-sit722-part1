a6a50b32ef29d4183c1f60559bdec958bab3e765
cjvrd <cjvirdo@gmail.com>
2024-08-21 23:24:29 +1000

    asdf

--- app/config.py
+++ app/config.py
@@ -1,6 +1,6 @@
 import os
 
 class Settings:
-    DATABASE_URL: str = os.getenv("postgresql://christianvirdo_sit722_part1_user:AFyr6BrSN0U2j05hMAHfz0ZkoJINinAH@dpg-cr2uef88fa8c73c2cnhg-a.oregon-postgres.render.com/christianvirdo_sit722_part1")
+    DATABASE_URL: str = os.getenv("DATABASE_URL")
 
 settings = Settings()
